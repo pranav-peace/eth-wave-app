1bd5960a7e26988aac75e4f61fc6ed8f82c2f311
Pranav <mujumdarpranav@gmail.com>
2022-01-04 20:11:36 +0530

    Funded the contract

--- scripts/run.js
+++ scripts/run.js
@@ -1,22 +1,25 @@
 const main = async () => {
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.1"),
+    });
     await waveContract.deployed();
     console.log('Contract Address: ', waveContract.address);
 
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
-    console.log('Total Waves: ', Number(waveCount));
+    // Get Contract Balance
+    let contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log("Contract Balance: ", hre.ethers.utils.formatEther(contractBalance));
 
+    // Send Wave
     let waveTransaction = await waveContract.wave('A message!');
     await waveTransaction.wait();
 
-    const [_, randomPerson] = await hre.ethers.getSigners();
-    waveTransaction = await waveContract.connect(randomPerson).wave('Another message :)');
-    await waveTransaction.wait();
+    // Let's see what changed
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log("Contract Balance: ", hre.ethers.utils.formatEther(contractBalance));
 
     let waveArray = await waveContract.getAllWaves();
-    console.log('Final wave count:', Number(waveCount));
+    console.log(waveArray);
 };
 
 const runMain = async () => {
